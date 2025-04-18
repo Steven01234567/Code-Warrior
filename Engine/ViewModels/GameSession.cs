589e73137b6de8bd1e66b74ba47eaf81dcc13312
Steven01234567 <steven.bin.chen17@gmail.com>
2025-04-18 17:40:20 -0400

    Added Player's Home Location

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -9,7 +9,8 @@ namespace Engine.ViewModels
 {
     public class GameSession
     {
-        public Player CurrentPlayer {  get; set; }
+        public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
 
         public GameSession()
         {
@@ -20,6 +21,13 @@ namespace Engine.ViewModels
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Gold = 1000000;
             CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your house";
+            CurrentLocation.ImageName = "/Engine;component/Images/Locations/PlayerHouse.png";
         }
     }
 }
