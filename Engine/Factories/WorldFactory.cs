5ce9abb37636610caffa94d065a600b4fa3c0d02
Steven01234567 <steven.bin.chen17@gmail.com>
2025-04-18 18:43:42 -0400

    Added Movement

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -14,6 +14,10 @@ namespace Engine.Factories
             World newWorld = new World();
 
             newWorld.AddLocation(0, 0, "Home", "This is your home", "/Engine;component/Images/Locations/PlayerHouse.png");
+            newWorld.AddLocation(-1, 0, "Ashembly Risky the Fifth, the Alchemist", "Try my potions...for a fair price hohoho...", "/Engine;component/Images/Locations/__NOT_MADE_YET__");
+            newWorld.AddLocation(0, 1, "Segmentius Faultius, the Trader", "Self-proclaimed most trustworthy trader, no one else offers better deals than me!", "/Engine;component/Images/Locations/__NOT_MADE_YET__");
+            newWorld.AddLocation(1, 0, "Cecil Ceaser Gaston, the Blacksmith", "Your local blacksmith, the best in town!", "/Engine;component/Images/Locations/__NOT_MADE_YET__");
+
             return newWorld;
         }
     }
