21872589b5981c5a510620d69affbcd4b314ee19
Steven01234567 <steven.bin.chen17@gmail.com>
2025-04-18 19:27:44 -0400

    Added GameItem Factory

---
+++ Engine/Factories/ItemFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    public static class ItemFactory
+    {
+        private static List<GameItem> _standardGameItems;
+
+        static ItemFactory()
+        {
+            _standardGameItems = new List<GameItem>();
+
+            _standardGameItems.Add(new Weapon(1001, "The C", 0, 5, 10));
+            _standardGameItems.Add(new Weapon(1002, "The C++", 100, 5, 10));
+            _standardGameItems.Add(new Weapon(1003, "The C#", 100, 10, 20));
+        }
+
+        public static GameItem createGameItem(int itemTypeID)
+        {
+            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
+
+            if (standardItem != null)
+            {
+                return standardItem.Clone();
+            }
+
+            return null;
+        }
+    }
+}
