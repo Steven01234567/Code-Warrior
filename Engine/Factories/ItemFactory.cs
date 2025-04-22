52008295fdead1b5f39c18803846d575bc740c8f
Steven01234567 <steven.bin.chen17@gmail.com>
2025-04-22 18:01:53 -0400

    Changed item gold values and monster drop rates

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -14,17 +14,17 @@ namespace Engine.Factories
         static ItemFactory()
         {
             _standardGameItems.Add(new Weapon(1001, "The C", 10, 4, 8, 90, 15));
-            _standardGameItems.Add(new Weapon(1002, "The C++", 50, 4, 16, 70, 5, "Cleave"));
-            _standardGameItems.Add(new Weapon(1003, "The C#", 500, 8, 16, 95, 25));
-            _standardGameItems.Add(new Weapon(1011, "The Java-lin", 300, 8, 16, 50, 20, "Pierce"));
+            _standardGameItems.Add(new Weapon(1002, "The C++", 50, 8, 16, 85, 5, "Cleave"));
+            _standardGameItems.Add(new Weapon(1003, "The C#", 1500, 16, 32, 95, 25));
+            _standardGameItems.Add(new Weapon(1011, "The Java-lin", 1200, 16, 32, 75, 20, "Pierce"));
             _standardGameItems.Add(new Potion(2001, "Health Potion", 10, "+50 HP"));
-            _standardGameItems.Add(new GameItem(7001, "Pytorch", 500));
-            _standardGameItems.Add(new GameItem(8001, "Rustmetal", 200));
-            _standardGameItems.Add(new GameItem(8002, "Polished Rustmetal", 500));
-            _standardGameItems.Add(new GameItem(9001, "Jar of Mips", 15));
-            _standardGameItems.Add(new GameItem(9002, "Branch", 5));
-            _standardGameItems.Add(new GameItem(9003, "Leaf", 100));
-            _standardGameItems.Add(new GameItem(9004, "Root", 250));
+            _standardGameItems.Add(new GameItem(7001, "Pytorch", 2000));
+            _standardGameItems.Add(new GameItem(8001, "Rustmetal", 80));
+            _standardGameItems.Add(new GameItem(8002, "Polished Rustmetal", 200));
+            _standardGameItems.Add(new GameItem(9001, "Jar of Mips", 5));
+            _standardGameItems.Add(new GameItem(9002, "Branch", 10));
+            _standardGameItems.Add(new GameItem(9003, "Leaf", 15));
+            _standardGameItems.Add(new GameItem(9004, "Root", 25));
         }
 
         public static GameItem CreateGameItem(int itemTypeID, int quantity = 1)
--- Engine/Factories/MonsterFactory.cs
+++ Engine/Factories/MonsterFactory.cs
@@ -23,14 +23,14 @@ namespace Engine.Factories
                     return mip;
 
                 case 102:
-                    Monster mips = new Monster("Horde of Mips", "Mips.png", 100, 100, 0, 3, 30, 25, 5);
+                    Monster mips = new Monster("Horde of Mips", "Mips.png", 100, 100, 0, 3, 100, 25, 5);
 
-                    AddLootItem(mips, 9001, 100, 5);
+                    AddLootItem(mips, 9001, 95, 5);
 
                     return mips;
 
                 case 201:
-                    Monster nodes = new Monster("Nodes", "Nodes.png", 64, 64, 4, 4, 16, 16, 4);
+                    Monster nodes = new Monster("Nodes", "Nodes.png", 64, 64, 4, 4, 128, 64, 4);
 
                     AddLootItem(nodes, 9002, 75, 1);
                     AddLootItem(nodes, 9003, 25, 1);
@@ -38,7 +38,7 @@ namespace Engine.Factories
                     return nodes;
 
                 case 202:
-                    Monster binaryTree = new Monster("Binary Tree", "BinaryTree.png", 256, 256, 16, 16, 64, 64, 1);
+                    Monster binaryTree = new Monster("Binary Tree", "BinaryTree.png", 128, 128, 16, 16, 1024, 256, 1);
 
                     AddLootItem(binaryTree, 9002, 100, 63);
                     AddLootItem(binaryTree, 9003, 50, 32);
@@ -47,12 +47,12 @@ namespace Engine.Factories
                     return binaryTree;
 
                 case 301:
-                    Monster python = new Monster("BOSS: The Python", "Python.png", 1000, 1000, 20, 20, 200, 200, 2);
+                    Monster python = new Monster("BOSS: The Python", "Python.png", 400, 400, 20, 20, 400, 400, 2);
 
                     return python;
 
                 case 302:
-                    Monster pythonDark = new Monster("BOSS: The Python in Darkness", "PythonDark.png", 1000, 1000, 99, 99, 200, 200, 2);
+                    Monster pythonDark = new Monster("BOSS: The Python in Darkness", "PythonDark.png", 400, 400, 99, 99, 400, 400, 2);
 
                     return pythonDark;
 
